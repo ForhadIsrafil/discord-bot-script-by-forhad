85b9eee1f3f2af33e9c4c09dc6f542a1ae965c63
forhadisrafil <forhadtuhin5088@gmail.com>
2022-09-06 22:48:01 +0600

    on message, add imoji on message

--- demo_pycord.py
+++ demo_pycord.py
@@ -8,6 +8,7 @@ env.read_env()  # read .env file, if it exists
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
+print("Bot is running............")
 
 
 @bot.command(name="ping")
@@ -31,7 +32,7 @@ async def hello(ctx, name: str = None):
     await ctx.respond(f"Hello {name}!")
 
 
-@bot.slash_command(name="member", description="member joint date",)
+@bot.slash_command(name="member", description="member joint date", )
 async def members(ctx, *, member: discord.Member):
     await ctx.respond(embed=discord.Embed(title=f'{member} joined on \'{member.joined_at.strftime("%A %B %Y")}\''))
     # await ctx.respond(f'{member} joined on \'{member.joined_at.strftime("%A %B %Y")}\'')
@@ -44,15 +45,24 @@ async def dm(ctx, *, user: discord.Member, message=None):
     await ctx.message.delete()
 
 
+@bot.event
+async def on_message(message):
+    # print(message)
+    if message.content == 'bull':
+        await message.add_reaction('👍')
 
-
-
-
-
-
-
-
-
+    # await ctx.respond()
 
 
 bot.run(env.str("TOKEN", ''))
+
+
+'''
+#react command
+@bot.command()
+@commands.has_permissions(manage_messages=True)
+async def react(ctx,emoji,message_id):
+  channel = ctx.channel
+  msg = await channel.fetch_message(message_id)
+  await msg.add_reaction(emoji)
+'''
